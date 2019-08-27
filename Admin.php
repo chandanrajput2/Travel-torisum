71221a3c6bf16ebfe5e0b567ae7cb3ec9f1176ff
chandanrajput2 <42267102+chandanrajput2@users.noreply.github.com>
2019-08-27 15:18:21 +0530

    Add files via upload

---
+++ Admin.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css" />
+  <link rel="stylesheet" href="css/style.css">
+  <title>Travel & torisum</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <div class="container">
+      <a href="index.html" class="navbar-brand">TRAVEL & TOURISM</a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="index.html" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item active">
+            <a href="about.html" class="nav-link">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a href="services.html" class="nav-link">Services</a>
+          </li>
+          <li class="nav-item">
+            <a href="blog.html" class="nav-link">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a href="contact.html" class="nav-link">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- PAGE HEADER -->
+  <header id="page-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 m-auto text-center">
+          <h1>About Us</h1>
+          <p>The land of Moksh - Varanasi is popularly known as Kashi and Banaras. This one of the holiest cities of India is situated in.</p>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- ABOUT SECTION -->
+  <section id="about" class="py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h1>What We Do</h1>
+          <p>Varanasi is located at an elevation of 80.71 metres (264.8 ft)[40] in the centre of the Ganges valley of 
+            North India, in the Eastern part of the state of Uttar Pradesh, along the left crescent-shaped bank of the Ganges,
+             averaging between 15 metres (50 ft) and 21 metres (70 ft) above the river.[41] The city is the headquarters of Varanasi district. By road, Varanasi is located 797 kilometres (495 mi) south-east of New Delhi, 320 kilometres (200 mi) south-east of Lucknow, 121 kilometres (75 mi) east of Allahabad, and 63 kilometres (39 mi) south of Jaunpur</p>
+          
+        </div>
+        <div class="col-md-6">
+          <img src="img/11.jpg" alt="" class="img-fluid rounded-circle d-none d-md-block about-img">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- ICON BOXES -->
+  <section id="icon-boxes" class="p-5">
+    <div class="container">
+      <div class="row mb-4">
+        <div class="col-md-4">
+          <div class="card bg-danger text-white text-center">
+            <div class="card-body">
+              <h3>Manikarnika Ghat</h3>
+              Manikarnika Ghat, the main burning ghat, is the most auspicious place for a Hindu to be cremated. Dead bodies are handled by outcasts known as doms, and are carried through the alleyways of the old town to the holy Ganges on a bamboo stretcher, swathed in cloth. The corpse is doused in the Ganges prior to cremation.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card bg-dark text-white text-center">
+            <div class="card-body">
+              
+              <h3>Dashashwamedh Ghat</h3>
+              Varanasi’s liveliest and most colourful ghat. The name indicates that Brahma sacrificed (medh) 10 (das) horses (aswa) here. In spite of the persistent boat owners, flower sellers, massage practitioners and touts trying to drag you off to a silk shop, it’s a wonderful place to linger and people-watch while soaking up the atmosphere. Every evening at 7pm an elaborate and popular ganga aarti (river worship) ceremony with puja (prayers), fire and dance is staged here.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card bg-danger text-white text-center">
+            <div class="card-body">
+              
+              <h3>Banares Hindu University</h3>
+              Long regarded as a centre of learning, Varanasi’s tradition of top-quality education continues today at Banares Hindu University, established in 1916. The wide, tree-lined streets and parkland of the 5-sq-km campus offer a peaceful atmosphere a world away from the city outside. On campus is Bharat Kala Bhavan, a roomy museum with a wonderful collection of miniature paintings, as well as 12th-century palm-leaf manuscripts, sculptures and local history displays.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mb-4">
+        <div class="col-md-4">
+          <div class="card bg-dark text-white text-center">
+            <div class="card-body">
+              
+              <h3>Vishwanath Temple</h3>
+              There are temples at almost every turn in Varanasi, but this is the most famous of the lot. It is dedicated to Vishveswara – Shiva as lord of the universe. The current temple was built in 1776 by Ahalya Bai of Indore; the 800kg of gold plating on the tower and dome was supplied by Maharaja Ranjit Singh of Lahore 50 years later.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card bg-danger text-white text-center">
+            <div class="card-body">
+              
+              <h3>Assi Ghat</h3>
+              The furthest south of the main ghats and one of the biggest, Assi Ghat is particularly important as the River Assi meets the Ganges near here and pilgrims come to worship a Shiva lingam (phallic image of Shiva) beneath a peepul tree. Evenings are particularly lively, as the ghat’s vast concreted area fills up with hawkers and entertainers during a small fire ceremony. It also features music and yoga at sunrise. It’s also a popular starting point for boat trips.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card bg-dark text-white text-center">
+            <div class="card-body">
+             
+              <h3>Harishchandra Ghat</h3>
+              Harishchandra Ghat is a cremation ghat – smaller and secondary in importance to Manikarnika, but one of the oldest ghats in Varanasi.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- TESTIMONIALS -->
+  <section id="testimonials" class="p-4 bg-dark text-white">
+    <div class="container">
+      <h2 class="text-center">testimonials</h2>
+      <div class="row text-center">
+        <div class="col">
+          <div class="slider">
+            <div>
+              <blockquote class="blockquote">
+                <p class="mb-0">
+                  The main cremation ghat; a must-see, but remain quiet and never take photographs (note: scams are plentiful here; see the "Staying Safe" section
+                </p>
+                <footer class="blockquote-footer">Manikarnika Ghat
+                  <cite title="Company 1"></cite>
+                </footer>
+              </blockquote>
+            </div>
+            <div>
+              <blockquote class="blockquote">
+                <p class="mb-0">
+                  known as the 'main' ghat, this is the site of the large evening aarti ceremony
+                </p>
+                <footer class="blockquote-footer">Dasaswamedh Ghat
+                  <cite title="Company 2"></cite>
+                </footer>
+              </blockquote>
+            </div>
+            <div>
+              <blockquote class="blockquote">
+                <p class="mb-0">
+                  the cremation place where Raja Harishchandra did the last rituals of his son.
+                </p>
+                <footer class="blockquote-footer">Harishchandra Ghat
+                  <cite title="Company 3"></cite>
+                </footer>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- FOOTER -->
+  <footer id="main-footer" class="text-center p-4">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <p>Copyright &copy;
+            <span id="year"></span> TRAVEL & TOURISM</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script>
+
+  <script>
+    // Get the current year for the copyright
+    $('#year').text(new Date().getFullYear());
+
+    $('.slider').slick({
+      infinite: true,
+      slideToShow: 1,
+      slideToScroll: 1
+    });
+  </script>
+</body>
+
+</html>
